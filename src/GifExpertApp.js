0141857dc8213e986f49c4d7e153dbe7de0f9f09
arielrse <arielrse@gmail.com>
2022-03-16 20:54:22 -0400

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Linux']);
+
+    // const handleAdd = () => {        
+    //     //setCategories( [...categories, 'Toronto'] );
+    //     setCategories( cats => [...cats, 'Toronto'] );
+    // }
+
+    return (
+      <>
+        <h2>GifExpertApp</h2>
+        
+        <AddCategory setCategories={ setCategories } />
+        <hr />
+        <ol>
+            { 
+                categories.map( category => 
+                  <GifGrid 
+                    key={ category }
+                    category={ category } 
+                  />
+                )
+            }
+        </ol>
+
+      </>
+    );
+  }
+  
+  export default GifExpertApp;
